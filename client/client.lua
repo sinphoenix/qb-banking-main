00798501a08ff53daf71d8828d6665ad2a1382af
sinphoenix <81183421+sinphoenix@users.noreply.github.com>
2023-02-21 15:25:14 -0500

    BUILD

---
+++ client/client.lua
@@ -0,0 +1,369 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local blips = {}
+
+-- Functions
+
+local function PlayATMAnimation(animation)
+    local playerPed = PlayerPedId()
+    if animation == 'enter' then
+        RequestAnimDict('amb@prop_human_atm@male@enter')
+        while not HasAnimDictLoaded('amb@prop_human_atm@male@enter') do
+            Wait(0)
+        end
+        TaskPlayAnim(playerPed, 'amb@prop_human_atm@male@enter', "enter", 1.0,-1.0, 3000, 1, 1, true, true, true)
+    end
+
+    if animation == 'exit' then
+        RequestAnimDict('amb@prop_human_atm@male@exit')
+        while not HasAnimDictLoaded('amb@prop_human_atm@male@exit') do
+            Wait(0)
+        end
+        TaskPlayAnim(playerPed, 'amb@prop_human_atm@male@exit', "exit", 1.0,-1.0, 3000, 1, 1, true, true, true)
+    end
+end
+
+local function createBlips()
+    for k, v in pairs(Config.BankLocations) do
+        blips[k] = AddBlipForCoord(tonumber(v.x), tonumber(v.y), tonumber(v.z))
+        SetBlipSprite(blips[k], Config.Blip.blipType)
+        SetBlipDisplay(blips[k], 4)
+        SetBlipScale  (blips[k], Config.Blip.blipScale)
+        SetBlipColour (blips[k], Config.Blip.blipColor)
+        SetBlipAsShortRange(blips[k], true)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(tostring(Config.Blip.blipName))
+        EndTextCommandSetBlipName(blips[k])
+    end
+end
+
+local function removeBlips()
+    for k, _ in pairs(Config.BankLocations) do
+        RemoveBlip(blips[k])
+    end
+    blips = {}
+end
+
+local function openAccountScreen()
+    QBCore.Functions.TriggerCallback('qb-banking:getBankingInformation', function(banking)
+        if banking ~= nil then
+            SetNuiFocus(true, true)
+            SendNUIMessage({
+                status = "openbank",
+                information = banking
+            })
+        end
+    end)
+end
+
+-- Events
+
+RegisterNetEvent("hidemenu", function()
+    SetNuiFocus(false, false)
+    SendNUIMessage({
+        status = "closeATM"
+    })
+end)
+
+RegisterNetEvent('qb-banking:client:updateBankInformation', function(banking)
+    SendNUIMessage({
+        status = "loadBankAccount",
+        information = banking
+    })
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+    createBlips()
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    removeBlips()
+end)
+
+RegisterNetEvent('qb-banking:transferError', function(msg)
+    SendNUIMessage({
+        status = "transferError",
+        error = msg
+    })
+end)
+
+RegisterNetEvent('qb-banking:successAlert', function(msg)
+    SendNUIMessage({
+        status = "successMessage",
+        message = msg
+    })
+end)
+
+RegisterNetEvent('qb-banking:openBankScreen', function()
+    openAccountScreen()
+end)
+
+local BankControlPress = false
+ local function BankControl()
+    CreateThread(function()
+        BankControlPress = true
+        while BankControlPress do
+            if IsControlPressed(0, 38) then
+                exports['qb-core']:KeyPressed()
+                TriggerEvent('qb-banking:openBankScreen')
+            end
+            Wait(0)
+        end
+    end)
+end
+
+CreateThread(function()
+    if Config.UseTarget then
+        for k, v in pairs(Config.Zones) do
+            exports["qb-target"]:AddBoxZone("Bank_"..k, v.position, v.length, v.width, {
+                name = "Bank_"..k,
+                heading = v.heading,
+                minZ = v.minZ,
+                maxZ = v.maxZ
+            }, {
+                options = {
+                    {
+                        type = "client",
+                        event = "qb-banking:openBankScreen",
+                        icon = "fas fa-university",
+                        label = "Access Bank",
+                    }
+                },
+                distance = 1.5
+            })
+        end
+    else
+        local bankPoly = {}
+        for k, v in pairs(Config.BankLocations) do
+            bankPoly[#bankPoly+1] = BoxZone:Create(vector3(v.x, v.y, v.z), 1.5, 1.5, {
+                heading = -20,
+                name="bank"..k,
+                debugPoly = false,
+                minZ = v.z - 1,
+                maxZ = v.z + 1,
+            })
+            local bankCombo = ComboZone:Create(bankPoly, {name = "bankPoly"})
+            bankCombo:onPlayerInOut(function(isPointInside)
+                if isPointInside then
+                    exports['qb-core']:DrawText(Lang:t('info.access_bank_key'),'left')
+                    BankControl()
+                else
+                    BankControlPress = false
+                    exports['qb-core']:HideText()
+                end
+            end)
+        end
+    end
+end)
+
+-- qb-target
+if Config.UseTarget then
+    CreateThread(function()
+        exports['qb-target']:AddTargetModel(Config.ATMModels, {
+            options = {
+                {
+                    event = 'qb-banking:server:enteratm',
+                    type = 'server',
+                    icon = "fas fa-credit-card",
+                    label = "Use ATM",
+                },
+            },
+            distance = 1.5,
+        })
+    end)
+end
+
+RegisterNetEvent('qb-banking:client:loadATM', function(cards)
+    if cards and cards[1] then
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed, true)
+        for _, v in pairs(Config.ATMModels) do
+            local hash = joaat(v)
+            local atm = IsObjectNearPoint(hash, playerCoords.x, playerCoords.y, playerCoords.z, 1.5)
+            if atm then
+                PlayATMAnimation('enter')
+                QBCore.Functions.Progressbar("accessing_atm", "Accessing ATM", 1500, false, true, {
+                    disableMovement = false,
+                    disableCarMovement = false,
+                    disableMouse = false,
+                    disableCombat = false,
+                }, {}, {}, {}, function() -- Done
+                    SetNuiFocus(true, true)
+                    SendNUIMessage({
+                        status = "openATMFrontScreen",
+                        cards = cards,
+                    })
+                end, function()
+                    QBCore.Functions.Notify("Failed!", "error")
+                end)
+            end
+        end
+    else
+        QBCore.Functions.Notify("Please visit a branch to order a card", "error")
+    end
+end)
+
+-- Callbacks
+
+RegisterNUICallback("NUIFocusOff", function()
+    SetNuiFocus(false, false)
+    SendNUIMessage({
+        status = "closeATM"
+    })
+    PlayATMAnimation('exit')
+end)
+
+RegisterNUICallback("playATMAnim", function()
+    local anim = 'amb@prop_human_atm@male@idle_a'
+    RequestAnimDict(anim)
+    while not HasAnimDictLoaded(anim) do
+        Wait(0)
+    end
+    TaskPlayAnim(PlayerPedId(), anim, "idle_a", 1.0,-1.0, 3000, 1, 1, true, true, true)
+end)
+
+RegisterNUICallback("doATMWithdraw", function(data)
+    if data then
+        TriggerServerEvent('qb-banking:server:doAccountWithdraw', data)
+    end
+end)
+
+RegisterNUICallback("loadBankingAccount", function(data)
+    QBCore.Functions.TriggerCallback('qb-banking:server:loadBankAccount', function(banking)
+        if banking and type(banking) == "table" then
+            SendNUIMessage({
+                status = "loadBankAccount",
+                information = banking
+            })
+        else
+            SetNuiFocus(false, false)
+            SendNUIMessage({
+                status = "closeATM"
+            })
+        end
+    end, data.cid, data.cardnumber)
+end)
+
+RegisterNUICallback("removeCard", function(data)
+    QBCore.Functions.TriggerCallback('qb-debitcard:server:deleteCard', function(hasDeleted)
+        if hasDeleted then
+            SetNuiFocus(false, false)
+            SendNUIMessage({
+                status = "closeATM"
+            })
+            QBCore.Functions.Notify('Card has been deleted.', 'success')
+        else
+            QBCore.Functions.Notify('Failed to delete card.', 'error')
+        end
+    end, data)
+end)
+
+-- NUI
+
+RegisterNetEvent("hidemenu", function()
+    SetNuiFocus(false, false)
+    SendNUIMessage({
+        status = "closebank"
+    })
+end)
+
+RegisterNetEvent('qb-banking:client:newCardSuccess', function(cardno, ctype)
+    SendNUIMessage({
+        status = "updateCard",
+        number = cardno,
+        cardtype = ctype
+    })
+end)
+
+-- NUI Callbacks
+
+RegisterNUICallback("NUIFocusOff", function(_, cb)
+    SetNuiFocus(false, false)
+    SendNUIMessage({
+        status = "closebank"
+    })
+    cb("ok")
+end)
+
+RegisterNUICallback("createSavingsAccount", function(_, cb)
+    TriggerServerEvent('qb-banking:createSavingsAccount')
+    cb("ok")
+end)
+
+RegisterNUICallback("doDeposit", function(data, cb)
+    if tonumber(data.amount) ~= nil and tonumber(data.amount) > 0 then
+        TriggerServerEvent('qb-banking:doQuickDeposit', data.amount)
+        openAccountScreen()
+        cb("ok")
+    end
+    cb(nil)
+end)
+
+RegisterNUICallback("doWithdraw", function(data, cb)
+    if tonumber(data.amount) ~= nil and tonumber(data.amount) > 0 then
+        TriggerServerEvent('qb-banking:doQuickWithdraw', data.amount, true)
+        openAccountScreen()
+        cb("ok")
+    end
+    cb(nil)
+end)
+
+RegisterNUICallback("doATMWithdraw", function(data, cb)
+    if tonumber(data.amount) ~= nil and tonumber(data.amount) > 0 then
+        TriggerServerEvent('qb-banking:doQuickWithdraw', data.amount, false)
+        openAccountScreen()
+        cb("ok")
+    end
+    cb(nil)
+end)
+
+RegisterNUICallback("savingsDeposit", function(data, cb)
+    if tonumber(data.amount) ~= nil and tonumber(data.amount) > 0 then
+        TriggerServerEvent('qb-banking:savingsDeposit', data.amount)
+        openAccountScreen()
+        cb("ok")
+    end
+    cb(nil)
+end)
+
+RegisterNUICallback("savingsWithdraw", function(data, cb)
+    if tonumber(data.amount) ~= nil and tonumber(data.amount) > 0 then
+        TriggerServerEvent('qb-banking:savingsWithdraw', data.amount)
+        openAccountScreen()
+        cb("ok")
+    end
+    cb(nil)
+end)
+
+RegisterNUICallback("doTransfer", function(data, cb)
+    if data ~= nil then
+        TriggerServerEvent('qb-banking:initiateTransfer', data)
+        cb("ok")
+    end
+    cb(nil)
+end)
+
+RegisterNUICallback("createDebitCard", function(data, cb)
+    if data.pin ~= nil then
+        TriggerServerEvent('qb-banking:createBankCard', data.pin)
+        cb("ok")
+    end
+    cb(nil)
+end)
+
+RegisterNUICallback("lockCard", function(_, cb)
+    TriggerServerEvent('qb-banking:toggleCard', true)
+    cb("ok")
+end)
+
+RegisterNUICallback("unLockCard", function(_, cb)
+    TriggerServerEvent('qb-banking:toggleCard', false)
+    cb("ok")
+end)
+
+RegisterNUICallback("updatePin", function(data, cb)
+    if data.pin and data.currentBankCard then
+        TriggerServerEvent('qb-banking:updatePin', data.currentBankCard, data.pin)
+        cb("ok")
+    end
+    cb(nil)
+end)
